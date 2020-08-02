9704ba8829a198216b8ffeeae0a3fd91691b3ae8
dolphius21 <johnrudolphlee@gmail.com>
2020-08-02 16:11:06 +0800

    organized classes

--- js/Player.js
+++ js/Player.js
@@ -1,10 +1,23 @@
 class Player {
-   constructor(name, color, token, isTurn = false) {
+   constructor(name, id, color, isTurn = false) {
       this.name = name;
       this.id = id;
       this.color = color;
-      this.token = token
       this.isTurn = isTurn;
-      this.tokens = [];
+      this.tokens = this.createTokens(21);
+   }
+
+   // Create token objects for player
+   // @param {integer} num - Number of token objects to be created
+   // @return {array} tokens - an array of new token objects
+   createTokens(number) {
+      const tokens = [];
+
+      for (let i = 0; i < number; i++) {
+         const token = new Token(this, i);
+         tokens.push(token);
+      }
+
+      return tokens;
    }
 }
